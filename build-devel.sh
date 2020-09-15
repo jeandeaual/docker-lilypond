ee045ece6af31b31c12a86f839bd773eae2a680e
Alexis Jeandeau <alexis.jeandeau@gmail.com>
2020-09-16 00:17:39 +0900

    Update the build and run scripts

--- build-devel.sh
+++ build-devel.sh
@@ -1,13 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-lilypond_version="2.21.5"
+lilypond_version="${1:-"2.21.6"}"
 
 docker build \
-    -t jeandeaual/lilypond-devel \
+    -t "jeandeaual/lilypond:latest" \
+    -t "jeandeaual/lilypond:devel" \
+    -t "jeandeaual/lilypond:${lilypond_version}" \
     --build-arg lilypond_version="${lilypond_version}" \
     .
 docker build \
-    -t jeandeaual/lilypond-ly2video-devel \
+    -t "jeandeaual/lilypond:devel-fonts" \
+    -t "jeandeaual/lilypond:${lilypond_version}-fonts" \
+    --build-arg lilypond_version="${lilypond_version}" \
+    --build-arg install_fonts="true" \
+    .
+docker build \
+    -t "jeandeaual/lilypond:devel-ly2video" \
+    -t "jeandeaual/lilypond:${lilypond_version}-ly2video" \
     --build-arg lilypond_version="${lilypond_version}" \
     --build-arg install_ly2video="true" \
     .
--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+lilypond_version="${1:-"2.20.0"}"
 
 docker build \
-    -t jeandeaual/lilypond-stable \
+    -t "jeandeaual/lilypond:stable" \
+    -t "jeandeaual/lilypond:${lilypond_version}" \
+    --build-arg lilypond_version="${lilypond_version}" \
     .
 docker build \
-    -t jeandeaual/lilypond-ly2video-stable \
+    -t "jeandeaual/lilypond:stable-fonts" \
+    -t "jeandeaual/lilypond:${lilypond_version}-fonts" \
+    --build-arg lilypond_version="${lilypond_version}" \
+    --build-arg install_fonts="true" \
+    .
+docker build \
+    -t "jeandeaual/lilypond:stable-ly2video" \
+    -t "jeandeaual/lilypond:${lilypond_version}-ly2video" \
+    --build-arg lilypond_version="${lilypond_version}" \
     --build-arg install_ly2video="true" \
     .
